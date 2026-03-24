d8a35aa2e2e35d7551c0d863abbd48dd126fbf39
Alfieeb <Ajb3510@gmail.com>
2026-03-24 11:57:32 +0000

    refined text and fixed functions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 
 space = "                             "
-wait = time.sleep(1)
+
+def wait():
+    time.sleep(1.5)
+
 playerHealth = 10
 Inventory = []
 
@@ -18,20 +21,20 @@ def gameSetup():
     playerName = input("Enter your player's name. ")
     print(f"Welcome to castle escape {playerName}!")
     print(space)
-    wait
+    wait()
 
     print("You awaken in the castle dungeon, chained and trapped. You need to find a way out…")
     print(space)
-    wait
+    wait()
     print("But escaping isn’t enough — the crown jewels still await your grasp.")
     print(space)
-    wait
+    wait()
     print("The air is damp and cold, and distant footsteps echo through the stone halls.")
     print(space)
-    wait
+    wait()
     print("Somewhere above, guards patrol, unaware of your daring plan.")
     print(space)
-    wait
+    wait()
 
 def dungeonRoom():
     ActionOne = ""
@@ -39,8 +42,14 @@ def dungeonRoom():
         ActionOne = input("Would you like to look around for now Y/N? ")
         if ActionOne.upper() == "Y":
             print("You spot the heavy dungeon lock securing the door.")
+            print(space)
+            wait()
             print("A loose rock lies on the floor — maybe it could be useful.")
+            print(space)
+            wait()
             print("From above, you hear footsteps echoing through the stone halls.")
+            print(space)
+            wait()
             break  # exit loop
         elif ActionOne.upper() == "N":
             print("You decide to stay put for now, listening to the footsteps above.")
@@ -51,12 +60,20 @@ def dungeonRoom():
     ActionTwo = ""
     while True:
         ActionTwo = input("Do you want to pick up a loose rock, Y/N? ")
+        print(space)
+        wait()
+
         if ActionTwo.upper() == "Y":
             Inventory.append("Rock")
             print("You pick up the rock — maybe it can help break the lock.")
+            print(space)
+            wait()
             break
-        elif ActionTwo.upper == "N":
+        elif ActionTwo.upper() == "N":
             print("You leave the rock on the floor for now.")
+            break
+        else:
+            print("Please enter Y or N")
 
 
 dungeonRoom()
